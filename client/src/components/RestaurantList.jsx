4e6e36aa0a19553b35b31d5b30113f3f8a94bd8d
Asim Williams <asimwilliams@google.com>
2025-03-03 22:20:59 -0500

    delete restaurant through frontend

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -14,6 +14,17 @@ const RestaurantList = (props) => {
 		fetchData();
 	}, []);
 
+	const handleDelete = async (id) => {
+		try {
+			const response = await RestaurantsApi.delete(`/${id}`);
+			setRestaurants(
+				restaurants.filter((restaurant) => {
+					return restaurant.id !== id;
+				})
+			);
+		} catch (error) {}
+	};
+
 	return (
 		<div className="list-group">
 			<table className="table table-hover table-dark">
@@ -40,7 +51,12 @@ const RestaurantList = (props) => {
 										<button className="btn btn-warning">Update</button>
 									</td>
 									<td>
-										<button className="btn btn-danger">Delete</button>
+										<button
+											onClick={() => handleDelete(restaurant.id)}
+											className="btn btn-danger"
+										>
+											Delete
+										</button>
 									</td>
 								</tr>
 							);
